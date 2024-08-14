6a94f05da4211c509c8b43bb56540eda2a3ba079
gary <nyoqhaaco@gmail.com>
2024-08-14 11:00:15 +0800

    8/14 updata

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        button.setOnClickListener(new Temp());
+    }
+
+
+    private class Temp implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            System.out.println("hello world");
+        }
+    }
+}
